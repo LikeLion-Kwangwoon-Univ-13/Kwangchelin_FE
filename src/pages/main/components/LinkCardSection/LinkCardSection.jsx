2bc03700ef7575dd2b4fff5b5abb15ad0a782fef
DongGyun <wooheuy5752@gmail.com>
2025-05-03 20:05:25 +0900

    feat: 메인 페이지 LinkCard 섹션 구현

---
+++ src/pages/main/components/LinkCardSection/LinkCardSection.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router'
+
+import { Icon } from '@/components/Icon/Icon'
+import { BANNER_LIST } from '@/pages/main/constants/bannerList'
+
+import styles from './LinkCardSection.module.css'
+
+export const LinkCardSection = () => {
+  return (
+    <nav className={styles.container}>
+      <Link to={'/restaurant'} className={styles.bannerMap}>
+        <span>맛집 지도</span>
+        <Icon className={styles.pingIcon} name={'map-location'} size={20} fill='#ffffff' />
+        <Icon name={'shape-arrow-right'} size={20} />
+      </Link>
+
+      <ul className={styles.bannerContainer}>
+        {BANNER_LIST.map(({ label, description, icon, to }) => (
+          <li key={label} className={styles.banner}>
+            <Link to={to} className={styles.header}>
+              <span>{label}</span>
+              <Icon name={icon} size={20} />
+            </Link>
+            <p className={styles.description}>{description}</p>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
