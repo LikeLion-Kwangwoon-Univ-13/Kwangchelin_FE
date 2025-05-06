ebc7be2cdc170db9a101db9ec25e6746cda9a017
DongGyun <wooheuy5752@gmail.com>
2025-05-06 13:28:05 +0900

    feat: 마커 등록

--- src/features/restaurant/ui/RestaurantMap/index.jsx
+++ src/features/restaurant/ui/RestaurantMap/index.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from 'react'
 
-import styles from './RestaurantMap.module.css'
+import mapPing from '@/assets/map/map-ping.svg'
+import { RESTAURANT_DUMMY_DATA } from '@/mock/restaurantDummyData'
 
-const { kakao } = window
+import styles from './RestaurantMap.module.css'
 
 export const RestaurantMap = () => {
   const [_, setMap] = useState(null)
 
   //처음 지도 그리기
   useEffect(() => {
+    if (!window.kakao) return
+
+    const { kakao } = window
+
     //지도를 담을 영역의 DOM 레퍼런스
     const container = document.getElementById('map')
 
@@ -19,6 +24,25 @@ export const RestaurantMap = () => {
     const kakaoMap = new kakao.maps.Map(container, options)
 
     setMap(kakaoMap)
+
+    const imageSize = new kakao.maps.Size(24, 24)
+    const imageOption = { offset: new kakao.maps.Point(20, 40) }
+    const markerImage = new kakao.maps.MarkerImage(mapPing, imageSize, imageOption)
+
+    RESTAURANT_DUMMY_DATA.forEach((markerInfo) => {
+      const markerPosition = new kakao.maps.LatLng(markerInfo.latitude, markerInfo.longitude)
+
+      // 마커를 생성합니다
+      const marker = new kakao.maps.Marker({
+        position: markerPosition,
+        title: markerInfo.title,
+        image: markerImage,
+      })
+
+      // 마커가 지도 위에 표시되도록 설정합니다
+
+      marker.setMap(kakaoMap)
+    })
   }, [])
 
   return (
