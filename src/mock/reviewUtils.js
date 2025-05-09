0acd2c8e9662396862437d41df13b3855c9fa0d7
DongGyun <wooheuy5752@gmail.com>
2025-05-09 14:40:06 +0900

    feat: mock 함수 주석 추가

--- src/mock/reviewUtils.js
+++ src/mock/reviewUtils.js
@@ -1,3 +1,8 @@
+/**
+ * 문자열 형식의 날짜를 밀리초 단위의 timestamp로 변환
+ * @param {string} dateString - 'yy.mm.dd. hh:mm' 형식의 문자열
+ * @returns {number} 밀리초 단위의 timestamp
+ */
 const parseDate = (dateString) => {
   const [datePart, timePart] = dateString.split('. ')
   const [year, month, day] = datePart.split('.').map(Number)
@@ -6,6 +11,12 @@ const parseDate = (dateString) => {
   return new Date(2000 + year, month - 1, day, hour, minute).getTime()
 }
 
+/**
+ * 리뷰 목록을 정렬 기준에 따라 정렬
+ * @param {Array} reviews - 리뷰 객체 배열 (각 객체는 date와 rating 속성을 포함)
+ * @param {'latest' | 'rating'} sortBy - 정렬 기준 ('latest'는 최신순, 'rating'은 평점순)
+ * @returns {Array} 정렬된 리뷰 배열
+ */
 export const getSortedReviews = (reviews, sortBy = 'latest') => {
   const copied = [...reviews]
 
