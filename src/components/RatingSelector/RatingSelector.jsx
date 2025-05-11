d0f46c9e2c8216e1a8da6f8845156583095a9a48
DongGyun <wooheuy5752@gmail.com>
2025-05-11 20:18:42 +0900

    feat: RatingSelector 주석 추가 및 불러오기 수정

--- src/components/RatingSelector/RatingSelector.jsx
+++ src/components/RatingSelector/RatingSelector.jsx
@@ -2,6 +2,16 @@ import { Icon } from '@/components/Icon/Icon'
 
 import styles from './RatingSelector.module.css'
 
+/**
+ * 1~5점까지 평점을 선택할 수 있는 별점 선택 컴포넌트
+ *
+ * @param {Object} props
+ * @param {number} props.rating - 현재 선택된 평점 (1~5)
+ * @param {(rating: number) => void} props.onRate - 평점 클릭 시 호출되는 콜백
+ * @param {number} props.iconSize - 아이콘 크기 (px 단위)
+ * @returns {JSX.Element}
+ */
+
 export const RatingSelector = ({ rating, onRate, iconSize }) => {
   const handleRate = (selectedRating) => {
     onRate(selectedRating)
